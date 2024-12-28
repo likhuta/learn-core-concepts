f3f48783bb1bd5c641e1fa60e8956a9a02cdca18
VENTION\Hanna.Likhuta <hanna.likhuta@ventionteams.com>
2024-12-28 21:23:41 +0100

    add composite pattern

--- src/patterns/index.ts
+++ src/patterns/index.ts
@@ -1,9 +1,11 @@
 import { launchExampleSingleton } from './singleton';
 import { launchExampleObserver } from './observer';
 import { launchExampleAdapter } from './adapter';
+import { launchExampleComposite } from './composite';
 
 export default function launchPatterns() {
-    launchExampleSingleton();
-    launchExampleObserver();
-    launchExampleAdapter();
+    // launchExampleSingleton();
+    // launchExampleObserver();
+    // launchExampleAdapter();
+    launchExampleComposite();
 }
