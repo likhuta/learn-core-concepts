814f51806c4964894342d549618b174a377019e0
VENTION\Hanna.Likhuta <hanna.likhuta@ventionteams.com>
2024-12-30 12:09:36 +0100

    add `builder` pattern

--- src/patterns/index.ts
+++ src/patterns/index.ts
@@ -2,10 +2,12 @@ import { launchExampleSingleton } from './singleton';
 import { launchExampleObserver } from './observer';
 import { launchExampleAdapter } from './adapter';
 import { launchExampleComposite } from './composite';
+import { launchExampleBuilder } from './builder';
 
 export default function launchPatterns() {
     // launchExampleSingleton();
     // launchExampleObserver();
     // launchExampleAdapter();
-    launchExampleComposite();
+    // launchExampleComposite();
+    launchExampleBuilder();
 }
